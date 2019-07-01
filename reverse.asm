89f6f97538c3545c120f47c977c3e081d6992618
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-01 18:20:16 -0500

    Update reverse.asm

--- reverse.asm
+++ reverse.asm
@@ -1,44 +1,16 @@
-section .text
-    global _start
-
-section .bss
-    buff resb 20
-
-section .data
-    msg db "hello, world!", 0
-    len equ $-msg
-
+; rax = buffer
+; rbx = string
+; rcx = length
 reverse:
-    mov esi, eax
-    mov edi, ebx
-
-    xor edx, edx
-    _loop:
-        mov eax, [edi+ecx]
-        mov [esi+edx], eax
-
-        inc edx
-        dec ecx
-
-        cmp ecx, -1
-        jne _loop
-
-    ret
-
-_start:
-    mov eax, buff
-    mov ebx, msg
-    mov ecx, len-1
-    call reverse
-
-    mov byte[buff+edx], 0xA
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buff
-    mov edx, len+1
-    int 0x80
-
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    xor rdx, rdx
+    dec rcx
+    .next_value:
+        push word [rbx+rcx]
+        pop word [rax+rdx]
+        cmp rcx, 0
+        je .end
+        dec rcx
+        inc rdx
+        jmp .next_value
+    .end:
+        ret
