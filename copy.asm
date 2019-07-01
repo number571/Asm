b6a120258491563581e56ece87660a77f873fafc
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-01 18:05:16 -0500

    Update copy.asm

--- copy.asm
+++ copy.asm
@@ -1,39 +1,13 @@
-section .text
-    global _start
-
-section .bss
-    _len resb 4
-    buff resb 20
-
-section .data
-    msg db "hello, world", 0xA, 0
-    len equ $-msg
-
+; rax = buffer
+; rbx = string
+; rcx = length
 copy:
-    mov [_len], ecx
-    xor edx, edx
-
-    __lp:
-        mov ecx, [ebx+edx]
-        mov [eax+edx], ecx 
-
-        inc edx
-        cmp edx, [_len]
-        jne __lp
-
-    ret
-
-_start:
-    mov eax, buff
-    mov ebx, msg
-    mov ecx, len
-    call copy
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buff
-    int 0x80
-
-    mov eax, 1
-    mov ebx, 0
-    int 0x80
+    .next_value:
+        push word [rbx+rcx]
+        pop word [rax+rcx]
+        cmp rcx, 0
+        je .end
+        dec rcx
+        jmp .next_value
+    .end:
+        ret
