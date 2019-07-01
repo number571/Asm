ce1903bb05422c4630f9afd55e96f4ffbf58fcee
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-01 17:56:36 -0500

    Create to_number.asm

---
+++ to_number.asm
@@ -0,0 +1,22 @@
+; input:
+; - rsi = string
+; - rcx = length
+; output:
+; - rax = number
+to_number:
+    xor rax, rax
+    cmp rcx, 0
+    je .end
+    .next_value:
+        dec rcx
+        movzx rbx, byte[rsi]
+        inc rsi
+        sub rbx, '0'
+        add rax, rbx
+        cmp rcx, 0
+        je .end
+        mov rbx, 10
+        mul rbx
+        jmp .next_value
+    .end:
+        ret
