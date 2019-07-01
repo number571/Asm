3fc89a3946dfb23a3e98168406d760ae6bd7d230
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-01 17:56:55 -0500

    Update to_string.asm

--- to_string.asm
+++ to_string.asm
@@ -1,74 +1,28 @@
-section .text
-    global _start
-
-section .bss
-    _buff resb 20
-    buff resb 20
-
-section .data
-    newline db 0xA
-
-reverse:
-    mov esi, eax
-    mov edi, ebx
-
-    xor edx, edx
-
-    _reverse_loop:
-        mov eax, [edi+ecx]
-        mov [esi+edx], eax
-
-        inc edx
-        dec ecx
-
-        cmp ecx, -1
-        jne _reverse_loop
-
-    ret
-
+; input:
+; - rax = number
+; - rsi = buffer
+; output:
+; - rcx = length
 to_string:
-    xor edi, edi
-    inc edi
-
-    _loop:
-        xor edx, edx
-        mov ecx, 10
-        div ecx
-
-        add edx, '0'
-        mov [_buff+edi], edx
-
-        inc edi
-        cmp eax, 0
-        jne _loop
-
-    mov byte[_buff], 0
-    mov edx, edi
-    inc edx
-
-    mov eax, ebx
-    mov ebx, _buff
-    mov ecx, edx
-    call reverse
-
-    ret
-
-_start:
-    mov eax, 571
-    mov ebx, buff
-    call to_string
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buff
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 0x80
-
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    xor rcx, rcx
+    .push_digit:
+        xor rdx, rdx
+        mov rbx, 10
+        div rbx
+        add rdx, '0'
+        push rdx
+        inc rcx
+        cmp rax, 0
+        je .pop_digit
+        jmp .push_digit
+    .pop_digit:
+        cmp rax, rcx
+        je .end
+        pop rdx
+        mov [rsi+rax], rdx
+        inc rax
+        jmp .pop_digit
+    .end:
+        mov rdx, 0
+        mov [rsi+rax], rdx
+        ret
