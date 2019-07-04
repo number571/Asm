eb4130db6308cbbb59cb373397eb920c9531e214
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-04 17:27:54 -0500

    Update to_string.asm

--- to_string.asm
+++ to_string.asm
@@ -1,11 +1,15 @@
 ; input:
 ; - rax = number
+; - rcx = buffer size
 ; - rsi = buffer
 ; output:
 ; - rcx = length
 to_string:
+    mov rdi, rcx
     xor rcx, rcx
     .push_digit:
+        cmp rcx, rdi
+        je .pop_digit
         xor rdx, rdx
         mov rbx, 10
         div rbx
@@ -16,12 +20,14 @@ to_string:
         je .pop_digit
         jmp .push_digit
     .pop_digit:
-        cmp rax, rcx
-        je .end
-        pop rdx
-        mov [rsi+rax], rdx
-        inc rax
-        jmp .pop_digit
+        xor rax, rax
+        .pop_digit_iter:
+            cmp rax, rcx
+            je .end
+            pop rdx
+            mov [rsi+rax], rdx
+            inc rax
+            jmp .pop_digit_iter
     .end:
         mov rdx, 0
         mov [rsi+rax], rdx
