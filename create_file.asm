2e42019da0b36687bad91303870878adcd54c93a
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-13 06:16:01 -0500

    Update create_file.asm

--- create_file.asm
+++ create_file.asm
@@ -4,9 +4,18 @@ entry _start
 filename db "just_file.txt", 0
 
 _start:
+    ; Create
     mov rax, 8
     mov rbx, filename
     mov rcx, 0777
+    int 0x80
+    
+    ; descriptor
+    push rax
+    
+    ; Close
+    mov rax, 6
+    pop rbx
     int 0x80
 
     call exit
