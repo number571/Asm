523cd9ef023fa9813396340f1003fe9f36ae1af2
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-13 06:11:51 -0500

    Create read_file.asm

---
+++ read_file.asm
@@ -0,0 +1,42 @@
+format ELF64 executable
+entry _start
+
+buffsize equ 20
+buffer rb buffsize
+
+filename db "just_file.txt", 0
+
+macro print msg, len {
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, msg
+    mov rdx, len
+    int 0x80
+}
+
+_start:
+    mov rax, 5
+    mov rbx, filename
+    mov rcx, 0
+    int 0x80
+    ; 0 = O_RDONLY
+    ; 1 = O_WRONLY
+    ; 2 = O_RDWR
+
+    ; descriptor
+    push rax
+
+    mov rax, 3
+    pop rbx
+    mov rcx, buffer
+    mov rdx, buffsize
+    int 0x80
+
+    print buffer, buffsize
+
+    call exit
+
+exit:
+    mov rax, 1
+    mov rbx, 0
+    int 0x80
