44494321978922351e8553a4920ef23d4ce15f8b
[#571] <31502293+Number571@users.noreply.github.com>
2019-07-13 07:00:24 -0500

    Update write_file.asm

--- write_file.asm
+++ write_file.asm
@@ -2,22 +2,30 @@ format ELF64 executable
 entry _start
 
 filename db "just_file.txt", 0
-contents db "hello, world", 0xA, 0
+contents db "hello, world!!!", 0xA, 0
 length = $-contents
 
 _start:
-    ; Create
-    mov rax, 8
+    mov rax, 5
     mov rbx, filename
-    mov rcx, 0777
+    mov rcx, 1
     int 0x80
 
     ; descriptor
     push rax
 
+    mov rax, 19
+    pop rbx
+    mov rcx, 0 ; move cursor
+    mov rdx, 0
+    ; SEEK_SET = 0
+    ; SEEK_CUR = 1
+    ; SEEK_END = 2
+    int 0x80
+
     ; Write
     mov rax, 4
-    pop rbx
+    ; rbx = descriptor
     mov rcx, contents
     mov rdx, length
     int 0x80
