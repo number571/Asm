0ee77c91109456117c2e75026d6238b62ae69d9e
[#571] <31502293+Number571@users.noreply.github.com>
2019-08-02 11:28:15 -0500

    Create print_char.asm

---
+++ print_char.asm
@@ -0,0 +1,34 @@
+format ELF64
+public _start
+
+section '.text' executable 
+_start:
+	mov rax, 'A'
+	call print_char
+	call exit
+
+section '.print_char' executable
+; rax = char
+print_char:
+	push rdx
+	push rcx
+	push rbx
+	push rax
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, rsp
+	mov rdx, 1
+	syscall
+
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	ret
+
+section '.exit' executable
+exit:
+	mov rax, 1
+	xor rbx, rbx
+	int 0x80
