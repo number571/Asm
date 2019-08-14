1fb9ef16f7be99f9bc42173938c61b44c2f31a59
[#571] <31502293+Number571@users.noreply.github.com>
2019-08-14 18:39:55 -0500

    Update main.asm

--- JustOS/main.asm
+++ JustOS/main.asm
@@ -1,15 +1,18 @@
 use16
 org 0x7C00 ; boot address
 
+; start os
 _start:
     call start
 
-msg db 'hello, world!', 0     
-len = $-msg
+; segment data
+    str_msg db "Operation system 0.0.1", 0
+    msg db 'hello, world!', 0
 
 start:
 ; null registers
     cli
+    xor dx, dx
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -21,19 +24,81 @@ start:
     mov ax, 0x02
     int 0x10
 
+; print string
+    mov ax, str_msg
+    call print_string
+
 ; set cursor
-    mov dx, 0x00
+    call new_line
+
+; print string
+    mov ax, msg
+    call print_string
+
+; ; set cursor
+;     call new_line
+
+; ; print string
+;     mov ax, msg
+;     call print_string
+
+    ret
+
+new_line:
+    push ax
+    push bx
+
     mov ah, 0x02
+    add dh, 1
     xor bh, bh
     int 0x10
 
-; print string
-    mov bp, msg             
-    mov cx, len
+    pop bx
+    pop ax
+    ret
+
+; | input:
+; ax = string
+print_string:
+    push ax
+    push bx
+    push cx
+    push bp
+
+    mov bp, ax
+
+    call length_string             
+    mov cx, ax
+
     mov bl, 04h
     mov ax, 0x1301
     int 0x10
+
+    pop bp
+    pop cx
+    pop bx
+    pop ax
     ret
+
+; | input
+; ax = string
+; | output
+; ax = length
+length_string:
+    push di
+    push bx
+    xor di, di
+    mov bx, ax
+    .next_iter:
+        cmp [bx+di], byte 0
+        je .close
+        inc di
+        jmp .next_iter
+    .close:
+        mov ax, di
+        pop bx
+        pop di
+        ret
 
 times(512-2-($-0x07C00)) db 0 ; align 512 bytes
 db 0x55, 0xAA ; end boot section
